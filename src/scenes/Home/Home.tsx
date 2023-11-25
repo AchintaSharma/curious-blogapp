dacfc5e818d8447f0bf03d08db6ee7793d73112c
Fujaiel Rahman <fujaielxx@gmail.com>
2023-11-25 20:18:40 +0530

    homepage halfly done

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -1,8 +1,47 @@
+
+import BigAdvertisement from "../../components/common/advertisement/BigAdvertisement.tsx";
+import SmallAd from "../../components/common/advertisement/SmallAd.tsx";
+import MainCard from "../../components/common/cards/MainCard.tsx";
+import NewsLetter from "../../components/common/cards/NewsLetter.tsx";
+import CommonCards from "../../components/common/cards/commonCards/CommonCards.tsx";
+
 const Home = () => (
-    <div className="flex w-screen h-screen flex-col items-center justify-center">
-      <h1 className="font-bold text-4xl">Welcome to Sufians Blog! </h1>
-      <p className=" mt-3">lets start blogging</p>
+  <div className="max-w-screen-xl mx-auto sm:mx-2 md:mx-4 lg:mx-8  my-4">
+    {/* grid starts here */}
+    <div className="grid grid-cols-12 gap-12">
+      {/* first grid */}
+      <div className=" col-span-7 border border-green-700">
+        {/* title */}
+        <h2 className=" font-semibold text-2xl text-Zomp">Popular Blogs </h2>
+        <MainCard />
+        <MainCard />
+        <MainCard />
+        <MainCard />
+      </div>
+
+      {/* /////////////////////////////////////////////////////////////// */}
+      {/* second grid */}
+      <div className=" col-span-5 border border-red-700">
+        {/* title */}
+        <h2 className=" font-semibold text-2xl text-Zomp">Popular Blogs </h2>
+
+        {/* news letter */}
+        <div className=" flex justify-end">
+          <NewsLetter />
+        </div>
+
+        {/* advertisement */}
+        <SmallAd />
+
+      </div>
     </div>
-  )
+
+    {/* Advertisement */}
+    <BigAdvertisement />
+
+    {/* Recomended */}
+    <CommonCards />
+  </div>
+);
 
 export default Home;
