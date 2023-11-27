679becab4d4c9f50454289d243bdb3989ff81c22
Achinta Sharma <23achinta@gmail.com>
2023-11-27 19:50:19 +0530

    refactor - similarblogs section

--- src/components/common/cards/SmallCard.tsx
+++ src/components/common/cards/SmallCard.tsx
@@ -1,28 +1,32 @@
 import { IoShareOutline } from "react-icons/io5";
-import { Profile2 } from "../../../assets/svgs/svg.ts"
+import { Profile2 } from "../../../assets/svgs/svg.ts";
 // import {format} from 'date-fns'
 
 const SmallCard = () => (
-    <div className=" max-w-[304px] px-4 py-4 bg-Zomp rounded-lg flex flex-col gap-2">
-        {/* card profile, name, category */}
-        <div className=" flex items-center gap-2">
-            <img src={Profile2} alt="profile" className=" w-6 h-6 cursor-pointer" />
-            <h4 className=" text-sm font-semibold text-white cursor-pointer">John Doe</h4>
-            <h5 className="text-sm font-medium text-white cursor-pointer">React</h5>
-        </div>
-        {/* card content */}
-        <div>
-            <h2 className=" text-lg font-semibold text-white cursor-pointer">React Best Practices</h2>
-        </div>
-        {/* card footer */}
-        <div className=" flex items-center justify-between">
-            <div className=" flex items-center gap-2">
-                <p className=" text-sm text-white">12 Nov</p>
-                <p className=" text-white text-sm">5 min read</p>
-            </div>
-            <IoShareOutline className="cursor-pointer text-xl text-white font-bold" />
-        </div>
+  <div className="px-4 py-4 bg-Zomp rounded-lg flex flex-col gap-2">
+    {/* card profile, name, category */}
+    <div className=" flex items-center gap-2">
+      <img src={Profile2} alt="profile" className=" w-6 h-6 cursor-pointer" />
+      <h4 className=" text-sm font-semibold text-white cursor-pointer">
+        John Doe
+      </h4>
+      <h5 className="text-sm font-medium text-white cursor-pointer">React</h5>
     </div>
-  )
+    {/* card content */}
+    <div>
+      <h2 className=" text-lg font-semibold text-white cursor-pointer">
+        React Best Practices
+      </h2>
+    </div>
+    {/* card footer */}
+    <div className=" flex items-center justify-between">
+      <div className=" flex items-center gap-2">
+        <p className=" text-sm text-white">12 Nov</p>
+        <p className=" text-white text-sm">5 min read</p>
+      </div>
+      <IoShareOutline className="cursor-pointer text-xl text-white font-bold" />
+    </div>
+  </div>
+);
 
-export default SmallCard
+export default SmallCard;
