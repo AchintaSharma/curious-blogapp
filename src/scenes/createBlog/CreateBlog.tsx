4be3f0dd94388b72e86c0f73afe0f4c204c1674b
Fujaiel Rahman <fujaielxx@gmail.com>
2023-12-03 12:17:18 +0530

    fetch blog added to ourstory and post page

--- src/scenes/createBlog/CreateBlog.tsx
+++ src/scenes/createBlog/CreateBlog.tsx
@@ -1,71 +1,65 @@
 const CreateBlog = () => (
-    <div className=" max-w-screen-sm  py-8 px-12 mx-auto  rounded-xl border border-gray-300  shadow-lg bg-white my-4">
-        <h1 className=" mb-4 font-bold">Creat Blog</h1>
+  <div className=" max-w-screen-sm  py-8 px-12 mx-auto  rounded-xl border border-gray-300  shadow-lg bg-white my-4">
+    <h1 className=" mb-4 font-bold">Creat Blog</h1>
 
-        <form className=" mx-auto">
-          {/* title input */}
-          <div className=" mb-4">
-            <input
-              type="text"
-              placeholder="Title"
-              className=" w-full h-12 pl-4 border border-Zomp rounded-lg"
-            />
-          </div>
-          {/* category input */}
-
-          {/* dropdown */}
-          <select className=" w-full h-12 border border-Zomp rounded-lg px-4 block my-4">
-            <option value="first">Category</option>
-            <option value="second">second</option>
-            <option value="third">third</option>
-          </select>
-
-          {/* <div className=" mb-2">
-            <input
-              type="text"
-              placeholder="Category"
-              className=" w-full h-12 pl-4 border border-SpaceCadet rounded-lg"
-            />
-          </div> */}
-          {/* content area */}
-
-          <div>
-            <textarea
-              placeholder="Content..."
-              cols={50}
-              rows={6}
-              className=" w-full  pl-4 pt-4 border border-Zomp rounded-lg"
-             />
-          </div>
-
-          {/* label */}
-          <h3 className=" font-semibold mt-2">Add Thumbnail</h3>
-
-          {/* image input */}
-          <div className="my-2">
-            <input
-              type="file"
-              className=" w-full pt-2 h-12 pl-4 border border-Zomp rounded-lg"
-            />
-          </div>
-
-          {/* radio */}
-          <div className="my-4">
-            <input type="checkbox" />{" "}
-            <span className=" font-semibold ml-2">Publish</span>
-          </div>
-
-          {/* submit button */}
-          <div className="my-2">
-            <button
-              className="w-lg py-2 px-8 text-white bg-SpaceCadet rounded-2xl  hover:bg-PurpleNavy font-medium relative shadow-md"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+    <form className=" mx-auto">
+      {/* title input */}
+      <div className=" mb-4">
+        <input
+          type="text"
+          placeholder="Title"
+          className=" w-full h-12 pl-4 border border-Zomp rounded-lg"
+        />
       </div>
-  );
+      {/* category input */}
+
+      {/* dropdown */}
+      <select className=" w-full h-12 border border-Zomp rounded-lg px-4 block my-4">
+        <option value="first">Category</option>
+        <option value="second">second</option>
+        <option value="third">third</option>
+      </select>
+
+      {/* content area */}
+
+      <div>
+        <textarea
+          placeholder="Content..."
+          cols={50}
+          rows={6}
+          className=" w-full  pl-4 pt-4 border border-Zomp rounded-lg"
+          style={{ resize: "none" }}
+        />
+      </div>
+
+      {/* label */}
+      <h3 className=" font-semibold mt-2">Add Thumbnail</h3>
+
+      {/* image input */}
+      <div className="my-2">
+        <input
+          type="file"
+          className=" w-full pt-2 h-12 pl-4 border border-Zomp rounded-lg"
+        />
+      </div>
+
+      {/* radio */}
+      <div className="my-4">
+        <input type="checkbox" />{" "}
+        <span className=" font-semibold ml-2">Publish</span>
+      </div>
+
+      {/* submit button */}
+      <div className="my-2">
+        <button
+          className="w-lg py-2 px-8 text-white bg-SpaceCadet rounded-2xl  hover:bg-PurpleNavy font-medium relative shadow-md"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+);
 
 export default CreateBlog;
--- src/scenes/postPage/PostPage.tsx
+++ src/scenes/postPage/PostPage.tsx
@@ -5,120 +5,138 @@ import { Profile2 } from "../../assets/svgs/svg.ts";
 import { Bigthumb1 } from "../../assets/images/images.ts";
 import BigAdvertisement from "../../components/common/advertisement/BigAdvertisement.tsx";
 import CommonCards from "../../components/common/cards/commonCards/CommonCards.tsx";
-// import SmallCard from "../../components/common/cards/SmallCard";
+import useBlogStore from "../../store/blogStore.ts";
+import { useEffect } from "react";
 
-const PostPage = () => (
-  <div className="max-w-screen-xl mx-auto sm:mx-4 md:mx-8 lg:mx-16 xl:mx-24">
-    {/* title */}
-    <h2 className=" font-semibold text-2xl my-4 ">
-      Top 10 tricky JavaScript questions that I used to ask in interviews.
-    </h2>
+const PostPage = () => {
 
-    {/* author profile pic, name, published date */}
-    <div className=" flex items-center gap-2 my-4">
-      <img src={Profile2} alt="profile" className=" w-8 h-8 cursor-pointer" />
-      <div>
-        <h4 className=" text-sm font-semibold  cursor-pointer">John Doe</h4>
-        <h5 className="text-sm text-gray-500 font-light  cursor-pointer">
-          Published . 12Nov
-        </h5>
+  // Array of blogs
+  const blogs = useBlogStore((state)=>state.blogs);
+
+  // showing all blogs
+  const getAllBlogs = useBlogStore((state)=> state.getAllBlog)
+
+  // useEffect hook is used to trigger the getBlog function when the component mounts, ensuring that the data is fetched.
+  useEffect(() => {
+    getAllBlogs();
+  }, [getAllBlogs]);
+
+  return (
+    <div className="max-w-screen-xl mx-auto sm:mx-4 md:mx-8 lg:mx-16 xl:mx-24 my-4">
+      {/* title */}
+      <h2 className=" font-semibold text-2xl my-4 ">
+        Top 10 tricky JavaScript questions that I used to ask in interviews.
+      </h2>
+
+      {/* author profile pic, name, published date */}
+      <div className=" flex items-center gap-2 my-4">
+        <img src={Profile2} alt="profile" className=" w-8 h-8 cursor-pointer" />
+        <div>
+          <h4 className=" text-sm font-semibold  cursor-pointer">John Doe</h4>
+          <h5 className="text-sm text-gray-500 font-light  cursor-pointer">
+            Published . 12Nov
+          </h5>
+        </div>
       </div>
-    </div>
-    {/* like, read, share and save */}
-    <div className=" flex items-center justify-between border-t-2 border-b-2 py-4 my-4">
-      <div className=" flex items-center gap-8">
-        <FaRegHeart className="cursor-pointer text-2xl  font-bold" />
-        <h3 className=" text-sm text-gray-500">5 min read</h3>
+      {/* like, read, share and save */}
+      <div className=" flex items-center justify-between border-t-2 border-b-2 py-4 my-4">
+        <div className=" flex items-center gap-8">
+          <FaRegHeart className="cursor-pointer text-2xl  font-bold" />
+          <h3 className=" text-sm text-gray-500">5 min read</h3>
+        </div>
+
+        {/* share and save */}
+        <div className=" flex items-center gap-8">
+          <IoShareOutline className="cursor-pointer text-2xl  font-bold" />
+          <MdOutlineBookmarkAdd className="cursor-pointer text-2xl" />
+        </div>
       </div>
 
-      {/* share and save */}
-      <div className=" flex items-center gap-8">
-        <IoShareOutline className="cursor-pointer text-2xl  font-bold" />
-        <MdOutlineBookmarkAdd className="cursor-pointer text-2xl" />
-      </div>
-    </div>
-
-    {/* Content starts here */}
-    <div className=" my-8">
-      <p className=" text-lg text-gray-600">
-        Some tricky interview scheduling questions you will face. These
-        questions seem easy, but there is something fishy about them. So today
-        I’m going to show you 10 tricky questions to ask yourself before a
-        programmer interview. Some tricky interview scheduling questions you
-        will face. These questions seem easy, but there is something fishy about
-        them. So today I’m going to show you 10 tricky questions to ask yourself
-        before a programmer interview.
-      </p>
-      {/* image and source */}
-      <div className=" my-8 flex flex-col gap-2">
-        <img src={Bigthumb1} alt="blog thumbnail" className=" cursor-pointer" />
-        <p className=" cursor-pointer text-sm text-gray-500">
-          Source: Unsplash
+      {/* Content starts here */}
+      <div className=" my-8">
+        <p className=" text-lg text-gray-600">
+          Some tricky interview scheduling questions you will face. These
+          questions seem easy, but there is something fishy about them. So today
+          I’m going to show you 10 tricky questions to ask yourself before a
+          programmer interview. Some tricky interview scheduling questions you
+          will face. These questions seem easy, but there is something fishy
+          about them. So today I’m going to show you 10 tricky questions to ask
+          yourself before a programmer interview.
+        </p>
+        {/* image and source */}
+        <div className=" my-8 flex flex-col gap-2">
+          <img
+            src={Bigthumb1}
+            alt="blog thumbnail"
+            className=" cursor-pointer"
+          />
+          <p className=" cursor-pointer text-sm text-gray-500">
+            Source: Unsplash
+          </p>
+        </div>
+        {/* more content */}
+        <p className=" text-lg text-gray-600 my-2">
+          Some tricky interview scheduling questions you will face. These
+          questions seem easy, but there is something fishy about them. So today
+          I’m going to show you 10 tricky questions to ask yourself before a
+          programmer interview. Some tricky interview scheduling questions you
+          will face. These questions seem easy, but there is something fishy
+          about them. So today I’m going to show you 10 tricky questions to ask
+          yourself before a programmer interview.
+        </p>
+        <p className=" text-lg text-gray-600 my-2">
+          Storing global state in useState useStateis only suitable to store
+          components local states. This could include input value, toggle flags
+          and etc. Global state belongs to the app as a whole, it doesn’t relate
+          only to one specific component. If your data is used within multiple
+          pages or widgets — consider putting it into a global state (React
+          Context, Redux, MobX and etc). Let’s go through that example. It is
+          really simple, but let’s assume that we are going to have a much more
+          complex app soon. So component hierarchy will be really deep and the
+          user state will be used all over the app. In that case, we should
+          separate our state into the global scope, so it can be easily accessed
+          from any point of the app (and we don’t have to pass props 20–40
+          levels down). Let’s go through that example. It is really simple, but
+          let’s assume that we are going to have a much more complex app soon.
+          So component hierarchy will be really deep and the user state will be
+          used all over the app. In that case, we should separate our state into
+          the global scope, so it can be easily accessed from any point of the
+          app (and we don’t have to pass props 20–40 levels down).
         </p>
       </div>
-      {/* more content */}
-      <p className=" text-lg text-gray-600 my-2">
-        Some tricky interview scheduling questions you will face. These
-        questions seem easy, but there is something fishy about them. So today
-        I’m going to show you 10 tricky questions to ask yourself before a
-        programmer interview. Some tricky interview scheduling questions you
-        will face. These questions seem easy, but there is something fishy about
-        them. So today I’m going to show you 10 tricky questions to ask yourself
-        before a programmer interview.
-      </p>
-      <p className=" text-lg text-gray-600 my-2">
-        Storing global state in useState useStateis only suitable to store
-        components local states. This could include input value, toggle flags
-        and etc. Global state belongs to the app as a whole, it doesn’t relate
-        only to one specific component. If your data is used within multiple
-        pages or widgets — consider putting it into a global state (React
-        Context, Redux, MobX and etc). Let’s go through that example. It is
-        really simple, but let’s assume that we are going to have a much more
-        complex app soon. So component hierarchy will be really deep and the
-        user state will be used all over the app. In that case, we should
-        separate our state into the global scope, so it can be easily accessed
-        from any point of the app (and we don’t have to pass props 20–40 levels
-        down). Let’s go through that example. It is really simple, but let’s
-        assume that we are going to have a much more complex app soon. So
-        component hierarchy will be really deep and the user state will be used
-        all over the app. In that case, we should separate our state into the
-        global scope, so it can be easily accessed from any point of the app
-        (and we don’t have to pass props 20–40 levels down).
-      </p>
-    </div>
-    {/* Content ends here */}
-    {/* Tags */}
-    <div className=" flex flex-col gap-4 my-8">
-      <h2 className=" font-semibold  text-2xl">Tags</h2>
-      <div className=" max-w-screen-sm flex gap-2">
-        <button
-          type="button"
-          className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
-        >
-          Technology
-        </button>
-        <button
-          type="button"
-          className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
-        >
-          Art
-        </button>
-        <button
-          type="button"
-          className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
-        >
-          Music
-        </button>
+      {/* Content ends here */}
+      {/* Tags */}
+      <div className=" flex flex-col gap-4 my-8">
+        <h2 className=" font-semibold  text-2xl">Tags</h2>
+        <div className=" max-w-screen-sm flex gap-2">
+          <button
+            type="button"
+            className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
+          >
+            Technology
+          </button>
+          <button
+            type="button"
+            className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
+          >
+            Art
+          </button>
+          <button
+            type="button"
+            className=" font-semibold text-sm bg-slate-200 px-4 py-2 rounded-lg"
+          >
+            Music
+          </button>
+        </div>
       </div>
+
+      {/* Advertisement */}
+      <BigAdvertisement />
+
+      {/* Similar blogs */}
+      <CommonCards blogs={blogs} />
     </div>
-
-  {/* Advertisement */}
-    <BigAdvertisement />
-
-    {/* Similar blogs */}   
-    <CommonCards />
-
-  </div>
-);
+  );
+};
 
 export default PostPage;
